06977cc2600f254e66b164e4b8ca59c04972b39e
Myroslav Sviderok <myroslav.sviderok@gmail.com>
2026-07-03 00:35:12 +0300

    Remove nightly version of nitro and try to prepare the build for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
 import { nitro } from 'nitro/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
-export default defineConfig({
-  server: { port: 3000 },
-  resolve: { tsconfigPaths: true, external: ['@dnd-kit/solid'] },
-  plugins: [nitro(), tailwindcss(), tanstackStart(), solidPlugin({ ssr: true })],
-  experimental: {
-    bundledDev: false,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    server: {
+      port: 3000,
+    },
+    build: {
+      sourcemap: true,
+    },
+    resolve: {
+      tsconfigPaths: true,
+      external: ['@dnd-kit/solid'],
+    },
+    plugins: [
+      nitro(),
+      tailwindcss(),
+      tanstackStart(),
+      solidPlugin({
+        ssr: true,
+        babel: {
+          sourceMaps: true,
+        },
+      }),
+    ],
+    environments: {
+      ssr: {
+        define: {
+          'process.env.CLERK_SECRET_KEY': JSON.stringify(env.CLERK_SECRET_KEY),
+          'process.env.CLERK_JWT_ISSUER_DOMAIN': JSON.stringify(env.CLERK_JWT_ISSUER_DOMAIN),
+          'process.env.VITE_CONVEX_URL': JSON.stringify(env.VITE_CONVEX_URL),
+          'process.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(env.VITE_CLERK_PUBLISHABLE_KEY),
+        },
+      },
+    },
+  }
 })
